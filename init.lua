236b7091eb98687a1abbf700a57f6157a3f2a8bd
yarn <yarn@noreply.localhost>
2026-05-16 00:29:05 -0400

    shiz

--- init.lua
+++ init.lua
@@ -131,6 +131,9 @@ vim.lsp.config.zls = {
     settings = {
         zls = {
             enable_autofix = true,
+            enable_argument_placeholders = false,
+            enable_build_on_save = true,
+            build_on_save_step = "check",
             zig_exe_path = os.getenv("HOME") .. "/bin/zig-dir/zig",
         },
     },
